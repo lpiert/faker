11ba0136bfd376705baa06dbb96dfc3c3af7db0b
lpiert <33169234+lpiert@users.noreply.github.com>
2018-09-02 21:57:49 +0800

    Delete shell脚本代码.bat

--- vpnOnCentos搭建/shell脚本代码.bat
+++
@@ -1,8 +0,0 @@
-yum -y install wget
-
-wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
-
-其他随便，加密 aes-256-cfb
-协议 auth_shal_v4_compatible
-混淆 plain
-
