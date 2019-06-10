0f8d681ffe20b5320f360f14b80ebd49b1c34d64
lpiert <33169234+lpiert@users.noreply.github.com>
2019-06-11 00:04:06 +0800

    server instll
    v2ray openvpn ..

---
+++ all.sh
@@ -0,0 +1,15 @@
+#install openvpn
+#wget https://git.io/vpn -O openvpn-install.sh && bash openvpn-install.sh
+# set localtime
+#sudo cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+#install v2ray
+#bash <(curl -L -s https://install.direct/go.sh)
+#https://intmainreturn0.com/v2ray-config-gen/#   json create
+#vi /etc/v2ray/config.json
+#sudo systemctl restart v2ray
+#service v2ray status
+
+#install ss
+#wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh 
+#install bbr_plus
+#wget -N --no-check-certificate "https://github.com/cx9208/bbrplus/raw/master/ok_bbrplus_centos.sh" && chmod +x ok_bbrplus_centos.sh && ./ok_bbrplus_centos.sh
